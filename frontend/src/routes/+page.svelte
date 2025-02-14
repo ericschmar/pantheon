c95e3c47d7e43e13bc6ca18758e4c6cbf88b5b62
Eric Schmar <ericschmar@gmail.com>
2025-02-14 13:37:07 -0500

    init commit

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Sidebar from "$lib/components/sidebar-page.svelte";
+import { invoke } from "@tauri-apps/api/core";
+
+let name = $state("");
+let greetMsg = $state("");
+
+async function greet(event: Event) {
+	event.preventDefault();
+	// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+	greetMsg = await invoke("greet", { name });
+}
+</script>
+
+<main>
+    <Sidebar />
+</main>
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"embed"
+	"log"
+	"runtime"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/logger"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+)
+
+//go:embed frontend/build
+var assets embed.FS
+
+//go:embed build/appicon.png
+var icon []byte
+
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:             "ldap-explorer-go",
+		Width:             1024,
+		Height:            768,
+		MinWidth:          400,
+		MinHeight:         400,
+		DisableResize:     false,
+		Fullscreen:        false,
+		Frameless:         runtime.GOOS != "darwin",
+		StartHidden:       false,
+		HideWindowOnClose: false,
+		BackgroundColour:  &options.RGBA{R: 255, G: 255, B: 255, A: 255},
+		AssetServer: &assetserver.Options{
+			Assets:     assets,
+			Handler:    nil,
+			Middleware: nil,
+		},
+		Menu:             nil,
+		Logger:           nil,
+		LogLevel:         logger.WARNING,
+		OnStartup:        app.startup,
+		OnDomReady:       app.domReady,
+		OnBeforeClose:    app.beforeClose,
+		OnShutdown:       app.shutdown,
+		WindowStartState: options.Normal,
+		CSSDragProperty:  "--wails-draggable",
+		CSSDragValue:     "drag",
+		Bind: []interface{}{
+			app,
+		},
+		// Windows platform specific options
+		// Windows: &windows.Options{
+		// 	WebviewIsTransparent: true,
+		// 	WindowIsTranslucent:  true,
+		// 	DisableWindowIcon:    true,
+		// 	// DisableFramelessWindowDecorations: false,
+		// 	WebviewUserDataPath: "",
+		// 	BackdropType:        windows.Acrylic,
+		// },
+		// Mac platform specific options
+		Mac: &mac.Options{
+			TitleBar: &mac.TitleBar{
+				TitlebarAppearsTransparent: false,
+				HideTitle:                  false,
+				HideTitleBar:               false,
+				FullSizeContent:            false,
+				UseToolbar:                 false,
+				HideToolbarSeparator:       true,
+			},
+			Appearance:           mac.NSAppearanceNameDarkAqua,
+			WebviewIsTransparent: true,
+			WindowIsTranslucent:  false,
+			About: &mac.AboutInfo{
+				Title:   "My Application",
+				Message: "© 2021 Me",
+				Icon:    icon,
+			},
+		},
+		Debug: options.Debug{
+			OpenInspectorOnStartup: true,
+		},
+	})
+
+	if err != nil {
+		log.Println(err)
+		log.Fatal(err)
+	}
+}
