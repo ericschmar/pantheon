cce314b10cb64fa1ee6a32fba64f283224fd0d32
Eric Schmar <ericschmar@gmail.com>
2025-04-24 18:04:45 -0400

    remove placeholders

--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -27,7 +27,7 @@ const TextInput = ({
           value ? '' : 'text-gray-400',
           'text-xs pl-4 pr-8 shadow-none w-full fv-style rounded-r bg-white dark:bg-[hsl(218,_13%,_10%)] dark:text-white border border-l-0 border-gray-300 dark:border-gray-400/20 placeholder:italic placeholder:opacity-90 dark:placeholder:opacity-40',
         )}
-        value={value ? value : placeholder}
+        value={value ? value : ''}
         onChange={(v) => onChange?.(v)}
         {...props}
       />
